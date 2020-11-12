bc18b00bcad26945cc91b45435c598406f0c6bd1
jzamss@gmail.com <jzamss@gmail.com>
2020-11-12 15:52:42 +0800

    filipizen-rptis: support advance payment option

--- web/filipizen-rptis/src/rptbilling/InitialInfo.js
+++ web/filipizen-rptis/src/rptbilling/InitialInfo.js
@@ -10,6 +10,9 @@ import {
   Error,
   BackLink,
   useData,
+  Label,
+  Integer,
+  Panel,
   Card
 } from 'rsi-react-web-components'
 
@@ -23,25 +26,43 @@ const InitialInfo = ({
   const [ctx, dispatch] = useData();
   const { txntype } = ctx;
 
-  const [error, setError] = useState()
-  const [loading, setLoading] = useState(false)
-  const [refno, setRefno] = useState()
+  const currentYear = new Date().getFullYear();
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [refno, setRefno] = useState();
+  const [billtoyear, setBilltoyear] = useState(currentYear+1);
+  const [mode, setMode] = useState("initial");
 
   const getBilling = async (billOptions = {}) => {
     const svc = await Service.lookupAsync(`${partner.id}:OnlineLandTaxBillingService`, "rpt")
     const params = { txntype, refno, ...billOptions }
+    if (mode === "initial-advance") {
+      params.billtoyear = billtoyear;
+    }
     return await svc.invoke("getBilling", params);
   }
 
-  const loadBill = (billOptions = {}) => {
+  const loadBill = () => {
+    if (mode === "initial-advance") {
+      if (billtoyear <= currentYear) {
+        setError("Advance year to pay should be greater than " + currentYear);
+        return;
+      }
+    }
+
     setLoading(true);
     setError(null);
-    getBilling(billOptions).then(bill => {
+    getBilling().then(bill => {
       dispatch({type: "SET_BILL", refno: refno, bill: bill.info});
       moveNextStep();
     }).catch(err => {
-      setError(err.toString());
-      setLoading(false)
+      if (/unpaid|full/ig.test(err)) {
+        setMode("initial-advance");
+      } else {
+        setError(err.toString());
+      }
+      setLoading(false);
     })
   }
 
@@ -52,14 +73,41 @@ const InitialInfo = ({
       <Subtitle>Initial Information</Subtitle>
       <Spacer />
       <Error msg={error} />
-      <Text
-        caption='Tax Declaration No.'
-        name='refno'
-        value={refno}
-        onChange={setRefno}
-        readOnly={loading}
-        autoFocus={true}
-      />
+      <Panel visibleWhen={mode === "initial"}>
+        <Text
+          caption='Tax Declaration No.'
+          name='refno'
+          value={refno}
+          onChange={setRefno}
+          readOnly={loading}
+          autoFocus={true}
+        />
+      </Panel>
+
+      <Panel visibleWhen={mode === "initial-advance"}>
+        <p>
+        The associated ledger is fully paid for the current year.<br/>
+        To pay in advance, specify the year and click Next button.
+        </p>
+        <Spacer />
+        <Panel>
+          <Text
+            caption='Tax Declaration No.'
+            name='refno'
+            value={refno}
+            onChange={setRefno}
+            readOnly={true}
+          />
+          <Integer
+            caption="Advance Year to Pay"
+            name="billtoyear"
+            value={billtoyear}
+            onChange={setBilltoyear}
+            thousandSeparator={false}
+            autoFocus={true}
+          />
+        </Panel>
+      </Panel>
       <ActionBar>
         <BackLink caption='Back' variant="text" action={movePrevStep} />
         <Button caption='Next' action={loadBill} loading={loading} disabled={loading} />
